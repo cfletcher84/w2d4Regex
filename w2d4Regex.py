b5830465dd2f1f850e108dd60cb41741bdad04a5
cfletcher84 <chris@soldbyfletch.com>
2024-03-29 08:02:30 -0500

    First commit

---
+++ w2d4Regex.py
@@ -0,0 +1,16 @@
+# Python Regular Expressions Mastery
+
+import re
+
+text = "Contact emails are: john.doe@example.com and jane.doe@example.com."
+emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}", text)
+print(emails)
+
+
+# Python Regular Expressions Deep Dive
+
+import re
+
+text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@(?!exclude\.com)[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", text)
+print(emails)
